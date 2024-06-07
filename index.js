6e5852af14d0be349161c026fe1afc1b7cc18a0b
Peter Chahid <peterdaaboul@outlook.com>
2024-06-07 19:36:48 +0300

    related data

--- index.js
+++ index.js
@@ -7,28 +7,51 @@ import { typeDefs } from "./schema.js";
 //db
 import db from "./_db.js";
 
-
 const resolvers = {
-    Query: {
-        games(){
-            return db.games
-        },
-        reviews(){
-            return db.reviews
-        },
-        authors(){
-            return db.authors
-        },
-        review(_, args){
-            return db.reviews.find((review) => review.id === args.id)
-        }
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review:{
+    author(parent){
+        return db.authors.find((a) => a.id === parent.author_id)
+    },
+    game(parent){
+        return db.games.find((g) => g.id === parent.game_id)
     }
-}
+  }
+};
 
 //server Setup
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 const { url } = startStandaloneServer(server, {
--- schema.js
+++ schema.js
@@ -3,21 +3,28 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
+        game_id : Int!
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     type Query {
         games:[Game]
         reviews:[Review]
         review(id: ID!): Review
+        game(id:ID!):Game
+        author(id:ID!):Author
         authors:[Author]
     }
 
