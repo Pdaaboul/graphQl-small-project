2476698f6392a117e475959754af6163a221c5ad
Peter Chahid <peterdaaboul@outlook.com>
2024-06-07 18:29:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+//types
+import { typeDefs } from "./schema.js";
+
+//db
+import db from "./_db.js";
+
+const resolvers = {
+    Query: {
+        games(){
+            return db.games
+        },
+        reviews(){
+            return db.reviews
+        },
+        authors(){
+            return db.authors
+        }
+    }
+}
+
+//server Setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const { url } = startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("server ready at port: ", 4000);
